abc4f990965f5961f9b53da6a296e0dd8fea37bc
iredmond <iredmond@aliendebian.home>
2019-08-22 08:33:23 +1000

    blah

--- ransac_mqtt_v4.py
+++ ransac_mqtt_v4.py
@@ -111,7 +111,6 @@ if __name__=="__main__":
             cofg = clouds.CofG(clouds.splitXY(cloud,0,360))
             coff = clouds.CofF(clouds.splitXY(cloud,0,360))
             force = clouds.forcePush(cloud)
-            print force
             forceResultant = clouds.CofG(clouds.splitXY(force,0,360))
             if showWalls:
                 for wall in walls:
@@ -132,7 +131,7 @@ if __name__=="__main__":
         screen.blit(myGraph.surface, (0,0))
         pygame.display.flip()
 
-        clock.tick(5)  # limit to 10 fps (plenty fast enough)
+        clock.tick(10)  # limit to 10 fps (plenty fast enough)
 
     client.loop_stop()
     pygame.quit()
